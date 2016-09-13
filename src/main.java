fb17b2391a941422c284a1ea197aaad06b8df140
theseanathan <seandarryanto@gmail.com>
2016-09-13 12:18:52 -0700

    First commit for NYU_Proj 1, created various empty classes.

---
+++ src/main.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class main {
+
+}
+
+class MyArrayList<E extends Comparable<E>> extends ArrayList<E>{
+	
+}
+
+class Name implements Comparable<Name>{
+	public Name(String name, String gender, int count){
+		
+	}
+
+	@Override
+	public int compareTo(Name o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public int equals(Name o){
+		
+		return 0;
+	}
+	
+	public String toString(){
+		String name = "";
+		
+		return name;
+	}
+}
